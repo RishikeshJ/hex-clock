e6824fabecbaec603c76544229142dfbd9d660eb
Rishi <rishikesh.a.j@gmail.com>
2020-09-21 10:28:34 -0500

    HexClock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[DatePipe]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'hex-clock';
+  public time = new Date();
+  public hex: string;
+
+
+  ngOnInit() {
+    setInterval(() => {
+      this.time = new Date();
+      this.hex = '#' + this.datepipe.transform(this.time, 'hhmmss');
+      console.log(this.hex);
+    }, 1);
+  }
+  constructor(public datepipe: DatePipe) {}
+
+
 }
+
+
