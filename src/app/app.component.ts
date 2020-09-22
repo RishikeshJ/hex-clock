c174c08640d2e895144408e9179337c185ea913b
Rishi <rishikesh.a.j@gmail.com>
2020-09-21 20:33:57 -0500

    added gradient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,17 +11,22 @@ export class AppComponent implements OnInit {
   title = 'hex-clock';
   public time = new Date();
   public hex: string;
+  public gradient: string;
+  bgImage:any;
 
 
   ngOnInit() {
     setInterval(() => {
       this.time = new Date();
       this.hex = '#' + this.datepipe.transform(this.time, 'hhmmss');
+      this.gradient = '#' + 90 + this.datepipe.transform(this.time, 'mss');
+      this.bgImage = this.sanitizer.bypassSecurityTrustStyle('linear-gradient(-225deg,' + this.hex + ' 50%, ' + this.gradient+ ' 80%)');
     }, 1);
   }
-  constructor(public datepipe: DatePipe) {}
+  constructor(public datepipe: DatePipe, private sanitizer: DomSanitizer) {
+    
+  }
 
+  
 
 }
-
-
